b08396b373fb9830a9a23478207c7d3098b33c10
NWaleed786 <Waleedmega786@gmail.com>
2026-09-25 09:36:02 +0200

    2 Mise à jour globale

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,22 +2,28 @@ package org.example
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-    println(changeCouleur("Hello","rouge"))
-    println(changeCouleur("World","bleu"))
-    println("Hello ${changeCouleur("my","jaune")} World")
-    println(changeCouleur("Truc","marron"))
+import org.example.dresseur.Entraineur
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+fun main() {
+    var joueur = Entraineur(1,"Sacha",100)
+    var rival = Entraineur(2,"Regis",200)
+
+        joueur.afficheDetail()
+        rival.afficheDetail()
+        joueur.argents+=50
+        joueur.afficheDetail()
+
 }
+
+/**
+ * Change la couleur du message donné selon le nom de la couleur spécifié.
+ * Cette fonction utilise les codes d'échappement ANSI pour appliquer une couleur à la sortie console. Si un nom de couleur
+ * non reconnu ou une chaîne vide est fourni, aucune couleur n'est appliquée.
+ *
+ * @param message Le message auquel la couleur sera appliquée.
+ * @param couleur Le nom de la couleur à appliquer (ex: "rouge", "vert", "bleu"). Par défaut c'est une chaîne vide, ce qui n'applique aucune couleur.
+ * @return Le message coloré sous forme de chaîne, ou le même message si aucune couleur n'est appliquée.
+ */
 fun changeCouleur(message: String, couleur:String=""): String {
     val reset = "\u001B[0m"
     val codeCouleur = when (couleur.lowercase()) {
@@ -31,5 +37,5 @@ fun changeCouleur(message: String, couleur:String=""): String {
         else -> "" // pas de couleur si non reconnu
     }
     return "$codeCouleur$message$reset"
-
 }
+
